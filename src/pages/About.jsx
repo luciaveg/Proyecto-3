a936279827be62ecb3b278664266d7dbc857cc41
Lucía vega <luciavf98@gmail.com>
2024-02-12 16:47:39 +0100

    mejoradas pag contact us y about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,17 @@
+import React from 'react';
+
 const About = () => {
-	
-	return (
-		<div className="message">
-		<h1 >
-		About Us
-		</h1>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis molestiae fugit, suscipit recusandae accusantium iusto sit dolorem illum tenetur autem maxime voluptatum sed nemo eligendi at dicta dolore repellat magni?</p>
-		</div>
-	);
+  return (
+    <div className="about-container">
+      <h1 className="about-title">About Us</h1>
+      <p className="about-description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+        molestiae fugit, suscipit recusandae accusantium iusto sit dolorem illum
+        tenetur autem maxime voluptatum sed nemo eligendi at dicta dolore
+        repellat magni?
+      </p>
+    </div>
+  );
 };
 
 export default About;
