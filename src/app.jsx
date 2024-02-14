08d10401cc65d5199c6c549614530fcb323be5ca
Lucía vega <luciavf98@gmail.com>
2024-02-14 17:44:27 +0100

    scroll top button

--- src/app.jsx
+++ src/app.jsx
@@ -1,44 +1,51 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-import { AuthProvider } from './context/authcontext';
+import { AuthProvider } from "./context/authcontext";
 
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
-import LoginForm from './components/loginForm/loginForm';
-import Logo from './components/logo/logo';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import LoginForm from "./components/LoginForm/LoginForm";
+import Logo from "./components/Logo/Logonoticias.png";
 
-import './app.css';
-import NavBar from './components/navBar/navBar';
-import Error from './pages/Error';
-import About from './pages/About';
-import RegisterForm from './components/registerForm/registerForm';
-import Contact from './pages/Contact';
+import "./app.css";
+import NavBar from "./components/NavBar/navBar";
+import Error from "./pages/Error";
+import About from "./pages/About";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
+import Contact from "./pages/Contact";
+import React from "react";
+import ScrollToTopButton from "./ScrollToTopButton";
 
+function App() {
+  return (
+    <>
+      <AuthProvider>
+        <Logo />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<Home />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
 
-
-
-function App(){
-
-    return (
-		<>
-		<AuthProvider>
-			<Logo/>
-		<NavBar/>
-		<Routes>
-			<Route path="/" element={<Home/>}/>
-			<Route path="/news" element={<Home/>}/>
-			<Route path="/login" element={<LoginForm/>}/>
-			<Route path="/register" element={<RegisterForm/>}/>
-			<Route path="/contact" element={<Contact/>}/>
-			<Route path="/about" element={<About/>}/>
-			
-			<Route path="/*" element={<Error/>}/>
-		</Routes>
-		</AuthProvider>
-		</>
-	);
+          <Route path="/*" element={<Error />} />
+        </Routes>
+      </AuthProvider>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to My App</h1>
+        </header>
+        <div style={{ height: "2000px" }}>
+          {/* Contenido largo para que el usuario necesite desplazarse */}
+        </div>
+        <ScrollToTopButton />
+      </div>
+    </>
+  );
 }
 export default App;
--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,12 +1,13 @@
-import './logo.css'
+import "./logo.css";
 
 const Logo = () => {
-    
-    return (
-        <div className="logo">
-            <img src="src/componets/logo/logonoticias.png" alt="Foto del LOGO de la App"/>
-        </div>
-        
-    )
-}
+  return (
+    <div className="logo">
+      <img
+        src="src/components/logo/logonoticias.png"
+        alt="Foto del LOGO de la App"
+      />
+    </div>
+  );
+};
 export default Logo;
