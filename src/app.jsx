5f7f3392b3a8aba572c8dc5a6e1d326cd609dc5e
Guscv <gustavoacardenasv@gmail.com>
2024-02-23 16:06:20 -0500

    Perfil del usuario

--- src/app.jsx
+++ src/app.jsx
@@ -18,6 +18,7 @@ import RegisterForm from './components/RegisterForm/RegisterForm';
 import Contact from './pages/Contact';
 import React from 'react';
 import ScrollToTopButton from './ScrollToTopButton';
+import { SfNav } from 'react-sf-building-blocks';
 
 
 function App(){
@@ -27,6 +28,110 @@ function App(){
 		
 		<AuthProvider>
 			<Logo/>
+			<div>
+    <SfNav
+        showProfile={true}
+        profilePicture="https://i.pinimg.com/736x/7f/79/6d/7f796d57218d9cd81a92d9e6e8e51ce4--free-avatars-online-profile.jpg"
+        profileMenu={[
+        { caption: 'Profile', link: 'profile' },
+        [
+            { caption: 'Settings', link: 'support' },
+            { caption: 'Notifications', link: 'notifications' },
+            { caption: 'Privacy', link: 'privacy' },
+        ],
+        { caption: 'Support', link: 'support' },
+        { caption: 'Upgrade', link: 'upgrade' },
+        ]}
+        profilePreamble={
+        <div
+            style={{
+              width: '100%',
+              paddingTop: '10px',
+              paddingBottom: '10px',
+              paddingLeft: '10px',
+              paddingRight: '10px',
+            }}
+        >
+            <small>Hi Hrushi!</small>
+            <br />
+            <small>
+            <small>
+                <i>Welcome to Superflows!</i>
+            </small>
+            </small>
+        </div>
+        }
+        profileComponent={
+        <div
+            style={{
+              width: '100%',
+              paddingTop: '10px',
+              paddingBottom: '10px',
+              paddingLeft: '10px',
+              paddingRight: '10px',
+            }}
+        >
+            <small>Sign Out</small>
+        </div>
+        }
+        signInCaption="Subscribe"
+        onMenuClicked={(value) => {
+        alert(value);
+        }}
+        onSignInPressed={() => {
+        alert('Subscribe pressed!');
+        }}
+        stylesProfilePictureContainer={{
+          backgroundColor: 'black',
+          padding: '2px',
+          color: 'white',
+          borderRadius: '5px',
+        }}
+        stylesProfilePreamble={{ backgroundColor: 'white', color: 'black' }}
+        stylesProfileComponent={{ backgroundColor: 'white', color: 'black' }}
+        stylesMenu={{
+          backgroundColor: 'black',
+          color: 'white',
+          paddingTop: '3px',
+          paddingBottom: '3px',
+          paddingLeft: '10px',
+          paddingRight: '10px',
+          borderRadius: '5px',
+        }}
+        stylesSubMenu={{
+          backgroundColor: 'black',
+          color: 'white',
+          paddingLeft: '5px',
+          paddingRight: '5px',
+          border: 'solid 1px gray',
+        }}
+        stylesMenuMobile={{
+          backgroundColor: 'black',
+          color: 'white',
+          paddingLeft: '5px',
+          paddingRight: '5px',
+          borderRadius: '5px',
+          border: 'solid 1px gray',
+        }}
+        stylesMenuMobileSelected={{
+          backgroundColor: 'white',
+          color: 'black',
+          paddingLeft: '5px',
+          paddingRight: '5px',
+          borderRadius: '5px',
+          border: 'solid 1px gray',
+        }}
+        stylesSubMenuMobile={{
+          backgroundColor: 'black',
+          color: 'white',
+          paddingLeft: '5px',
+          paddingRight: '5px',
+          borderRadius: '5px',
+          border: 'solid 1px gray',
+        }}
+    />
+      {/* Your Code */}
+    </div>
 		<NavBar/>
 		<Routes>
 			<Route path="/" element={<Home/>}/>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 //import React from 'react';
+import * as React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom/client';
 import App from './app.jsx';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
 
 // import { BrowserRouter } from 'react-router-dom';
 
